cb8df63a1e3b702256a872329fbb5db91031d714
mohamedragab33 <moha.ragab33@gmail.com>
2024-06-05 16:51:45 +0300

    Enhancement and resolving issues

--- src/main/java/com/microServiceArch/order_service/dto/OrderRes.java
+++ src/main/java/com/microServiceArch/order_service/dto/OrderRes.java
@@ -3,6 +3,8 @@ package com.microServiceArch.order_service.dto;
 
 import lombok.Builder;
 
+import java.util.List;
+
 @Builder
-public record OrderRes() {
-}
+public record OrderRes(Long id, String orderNumber, List<OrderLineItemsRes> orderLineItems) {}
+
